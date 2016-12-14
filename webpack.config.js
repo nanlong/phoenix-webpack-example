c4f6334acd83d7f9fe4a4ce598e8112570732c10
Jeff <200006506@qq.com>
2016-12-14 13:36:51 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,10 @@ module.exports = {
       {
         test: /\.js$/,
         loader: "babel-loader",
-        exclude: /node_modules/
+        exclude: /node_modules/,
+        query: {
+          presets: ["es2015"]
+        }
       },
       {
         test: /\.(css|scss|sass)$/,
